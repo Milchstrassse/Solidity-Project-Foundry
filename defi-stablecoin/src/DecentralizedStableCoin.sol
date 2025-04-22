b9c42bd7737e702ee0b4e0babc656d2b20f1bfea
Milchstrassse <jclygzf@163.com>
2025-04-22 19:22:14 +0800

    add:完成token铸造和燃烧功能

--- defi-stablecoin/src/DecentralizedStableCoin.sol
+++ defi-stablecoin/src/DecentralizedStableCoin.sol
@@ -23,6 +23,10 @@
 
 pragma solidity ^0.8.19;
 
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
 /*
  * @title DecentralizedStableCoin
  * @author Patrick Collins
@@ -34,12 +38,41 @@ pragma solidity ^0.8.19;
 * This is the contract meant to be owned by DSCEngine. It is a ERC20 token that can be minted and burned by the
 DSCEngine smart contract.
  */
-contract DecentralizedStableCoin {
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
     // Errors
+    error DSC__MustBeMoreThanZero();
+    error DSC__BurnAmountExceedsBalance();
+    error DSC__NotZeroAddress();
 
+    constructor() ERC20("Decentralized Stable Coin", "DSC") {
+        // constructor code
 
-    constructor() {
-        
     }
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount <= 0) {
+            revert DSC__MustBeMoreThanZero();
+        }
+        if (balance < _amount) {
+            revert DSC__BurnAmountExceedsBalance();
+        }
+
+        super.burn(_amount);
+    }
+
+    function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert DSC__NotZeroAddress();
+        }
+
+        if (_amount <= 0) {
+            revert DSC__MustBeMoreThanZero();
+        }
+        _mint(_to, _amount);
+        return true;
+    }
+
+
 
 }
