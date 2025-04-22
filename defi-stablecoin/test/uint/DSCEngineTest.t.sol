e5e201ad5feb14feda643feb3bb4279570ad12e2
Milchstrassse <jclygzf@163.com>
2025-04-22 23:59:48 +0800

    add:添加部署脚本以及完成price的单元测试

---
+++ defi-stablecoin/test/uint/DSCEngineTest.t.sol
@@ -0,0 +1,68 @@
+pragma solidity ^0.8.19;
+
+import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import { ERC20Mock } from "@openzeppelin/contracts/mocks/ERC20Mock.sol"; 
+// import { ERC20Mock } from "../mocks/ERC20Mock.sol";
+// import { MockV3Aggregator } from "../mocks/MockV3Aggregator.sol";
+// import { MockMoreDebtDSC } from "../mocks/MockMoreDebtDSC.sol";
+// import { MockFailedMintDSC } from "../mocks/MockFailedMintDSC.sol";
+// import { MockFailedTransferFrom } from "../mocks/MockFailedTransferFrom.sol";
+// import { MockFailedTransfer } from "../mocks/MockFailedTransfer.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {StdCheats} from "forge-std/StdCheats.sol";
+
+contract DSCEngineTest is StdCheats, Test {
+    DSCEngine dsce;
+    DecentralizedStableCoin dsc;
+    DeployDSC deployer;
+    HelperConfig helperConfig;
+
+    address public ethUsdPriceFeed;
+    address public btcUsdPriceFeed;
+    address public weth;
+    address public wbtc;
+    uint256 public deployerKey;
+
+    uint256 amountCollateral = 10 ether;
+    uint256 amountToMint = 100 ether;
+    address public user = makeAddr("Milchstrasse");
+
+    uint256 public constant STARTING_USER_BALANCE = 10 ether;
+    uint256 public constant MIN_HEALTH_FACTOR = 1e18;
+    uint256 public constant LIQUIDATION_THRESHOLD = 50;
+
+    function setUp() external {
+        deployer = new DeployDSC();
+        (dsc, dsce, helperConfig) = deployer.run();
+        (ethUsdPriceFeed, btcUsdPriceFeed, weth, wbtc, deployerKey) = helperConfig.activeNetworkConfig();
+
+        ERC20Mock(weth).mint(user, STARTING_USER_BALANCE);
+    }
+
+    // price test
+    function testGetUsdValue() public view {
+        uint256 ethAmount = 15e18;
+        // 15e18 ETH * $2000/ETH = $30,000e18
+        uint256 expectedUsd = 30_000e18;
+        uint256 usdValue = dsce.getUsdValue(weth, ethAmount);
+        assertEq(usdValue, expectedUsd);
+    }
+
+    /**
+    * @dev 测试当抵押品数量为 0 时，depositCollateral 函数是否会触发回滚
+    */
+    function testRevertsIfCollateralZero() public {
+        vm.startPrank(user);
+        // 调用 ERC20Mock 合约的 approve 方法，允许 DSCEngine 合约（地址为 address(dsce)）
+        // 代表当前用户（user）花费 amountCollateral 数量的 WETH 代币
+        ERC20Mock(weth).approve(address(dsce), amountCollateral);
+
+        vm.expectRevert(DSCEngine.DSCEngine__NeedsMoreThanZero.selector);
+        // 调用 DSCEngine 合约的 depositCollateral 函数，尝试存入数量为 0 的 WETH 作为抵押品
+        dsce.depositCollateral(weth, 0);
+        vm.stopPrank();
+    }
+}
